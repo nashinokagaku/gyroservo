eba13ab283712bd09c9c4690abd56a25fd8069e1
nashinokagaku <uecnonashi@gmail.com>
2022-02-26 01:38:02 +0900

    add dmp processing

--- gyroservo.ino
+++ gyroservo.ino
@@ -1,129 +1,196 @@
 #include <Wire.h>
 #include <Servo.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
-#define MPU6050_ADDR 0x68
-#define MPU6050_AX  0x3B
-#define MPU6050_AY  0x3D
-#define MPU6050_AZ  0x3F
-#define MPU6050_TP  0x41
-#define MPU6050_GX  0x43
-#define MPU6050_GY  0x45
-#define MPU6050_GZ  0x47
-
-short int AccX, AccY, AccZ;
-short int Temp;
-short int GyroX, GyroY, GyroZ;
-float AccX_f = 0.0f, AccY_f = 0.0f, AccZ_f = 0.0f;
-float GyroX_f = 0.0f, GyroY_f = 0.0f, GyroZ_f = 0.0f;
-static String key;
+MPU6050 mpu;
 Servo servo1;
 Servo servo2;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+static String key;
 const int SV_PIN1 = 6;
 const int SV_PIN2 = 7;
-const float g = 9.81;
 
 void setup(){
-  Serial.begin( 9600 );
-  Wire.begin();
-  Wire.beginTransmission(MPU6050_ADDR);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission();
-  Serial.print(""); Serial.print("AccX");
-  Serial.print(","); Serial.print("AccY");
-  Serial.print(","); Serial.print("AccZ");
-//  Serial.print(","); Serial.print("GyroX");
-//  Serial.print(","); Serial.print("GyroY");
-//  Serial.print(","); Serial.print("GyroZ"); 
-  Serial.println("");
-  servo1.attach(SV_PIN1);
-  servo2.attach(SV_PIN2);
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXAccelOffset(86);
+    mpu.setYAccelOffset(1206);
+    mpu.setZAccelOffset(1500);// 1688 factory default for my test chip
+    mpu.setXGyroOffset(38);
+    mpu.setYGyroOffset(-89);
+    mpu.setZGyroOffset(-58); 
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+    servo1.attach(SV_PIN1);
+    servo2.attach(SV_PIN2);
+    servo1.write(90);
+    servo2.write(90);
 }
 
 void interpolation(float d_pos1, float d_pos2, int d_time, int d_num){
-  int i = 0;
-  float pos[2];
-  float tpos[2];
-  float hpos[2];
-  float dpos[2];
-  hpos[0] = 60;
-  hpos[1] = 60;
-  dpos[0] = d_pos1;
-  dpos[1] = d_pos2;
-  tpos[0] = hpos[0] + dpos[0];
-  tpos[1] = hpos[1] + dpos[1];
-  pos[0] = servo1.read();
-  pos[1] = servo2.read();
-  tpos[0] -= pos[0];
-  tpos[1] -= pos[1];
-  tpos[0] /= d_num;
-  tpos[1] /= d_num;
-  for(i = 0; i < d_num; i++){
-  	pos[0] = pos[0] + tpos[0];
-  	pos[1] = pos[1] + tpos[1];
-  	if (pos[0] >= 120){
-		  pos[0] = 120;
-	  }
-    else if (pos[0] <= 0){
-      pos[0] = 0;
+    int i = 0;
+    float pos[2];
+    float tpos[2];
+    float hpos[2];
+    float dpos[2];
+    hpos[0] = 90;
+    hpos[1] = 90;
+    dpos[0] = d_pos1;
+    dpos[1] = d_pos2;
+    tpos[0] = hpos[0] + dpos[0];
+    tpos[1] = hpos[1] + dpos[1];
+    pos[0] = servo1.read();
+    pos[1] = servo2.read();
+    tpos[0] -= pos[0];
+    tpos[1] -= pos[1];
+    tpos[0] /= d_num;
+    tpos[1] /= d_num;
+    for(i = 0; i < d_num; i++){
+        pos[0] = pos[0] + tpos[0];
+        pos[1] = pos[1] + tpos[1];
+        if (pos[0] >= 180){
+        	pos[0] = 180;
+        }
+        else if (pos[0] <= 0){
+        	pos[0] = 0;
+        }
+        if (pos[1] >= 180){
+        	pos[1] = 180;
+        }
+        else if (pos[1] <= 0){
+        	pos[1] = 0;
+        }
+        servo1.write(pos[0]);
+        servo2.write(pos[1]);
+        delay(d_time);
     }
-    if (pos[1] >= 120){
-      pos[1] = 120;
-    }
-    else if (pos[1] <= 0){
-      pos[1] = 0;
-    }
-  	servo1.write(pos[0]);
-  	servo2.write(pos[1]);
-    // Serial.println(pos[0]);
-    delay(d_time);
-  }
 }
 
 void loop(){
-  if ( Serial.available() ){
-    key = Serial.readString();
-//    Serial.write( key );
-  }
-//  Serial.println( key );
-  Wire.beginTransmission(MPU6050_ADDR);
-  Wire.write(MPU6050_AX);
-  Wire.endTransmission();  
-  Wire.requestFrom(MPU6050_ADDR, 14);
-  AccX = Wire.read() << 8;  AccX |= Wire.read();
-  AccY = Wire.read() << 8;  AccY |= Wire.read();
-  AccZ = Wire.read() << 8;  AccZ |= Wire.read();
-  Temp = Wire.read() << 8;  Temp |= Wire.read();
-  GyroX = Wire.read() << 8; GyroX |= Wire.read();
-  GyroY = Wire.read() << 8; GyroY |= Wire.read();
-  GyroZ = Wire.read() << 8; GyroZ |= Wire.read();
-//  AccX_f = 0.9 * AccX_f + 0.95 * 9.81 * 4.0 / 65536 * 0.1 * (AccX + 520.2591200);
-//  AccY_f = 0.9 * AccY_f + 0.95 * 9.81 * 4.0 / 65536 * 0.1 * (AccY - 522.8294176);
-//  AccZ_f = 0.9 * AccZ_f + 0.95 * 9.81 * 4.0 / 65536 * 0.1 * (AccZ + 1052.728278);
-//  GyroX_f = 0.9 * GyroX_f + 500.0 / 65536 * 0.1 * (GyroX + 182.20048312);
-//  GyroY_f = 0.9 * GyroY_f + 500.0 / 65536 * 0.1 * (GyroY - 375.927516203);
-//  GyroZ_f = 0.9 * GyroZ_f + 500.0 / 65536 * 0.1 * (GyroZ - 213.864375375);
-  AccX_f = 0.8 * AccX_f + 0.95 * 9.81 * 4.0 / 65536 * 0.2 * (AccX + 520.2591200);
-  AccY_f = 0.8 * AccY_f + 0.95 * 9.81 * 4.0 / 65536 * 0.2 * (AccY - 522.8294176);
-  AccZ_f = 0.8 * AccZ_f + 0.95 * 9.81 * 4.0 / 65536 * 0.2 * (AccZ + 1052.728278);
-  GyroX_f = 0.8 * GyroX_f + 500.0 / 65536 * 0.2 * (GyroX + 182.20048312);
-  GyroY_f = 0.8 * GyroY_f + 500.0 / 65536 * 0.2 * (GyroY - 375.927516203);
-  GyroZ_f = 0.8 * GyroZ_f + 500.0 / 65536 * 0.2 * (GyroZ - 213.864375375);
-  Serial.print(""); Serial.print(AccX_f, 5);
-  Serial.print(","); Serial.print(AccY_f, 5);
-  Serial.print(","); Serial.print(AccZ_f, 5);
-//  Serial.print(","); Serial.print(GyroX_f, 5);
-//  Serial.print(","); Serial.print(GyroY_f, 5);
-//  Serial.print(","); Serial.print(GyroZ_f, 5); 
-//  Serial.println("");
-  if (key == "O"){
-    Serial.print(","); Serial.print( "ON" ); Serial.println("");
-    interpolation(90 * AccZ_f / g, 90 * AccY_f / g, 5, 5);
-  }
-  else{
-    Serial.print(","); Serial.print( "OFF" ); Serial.println("");
-    servo1.write(60);
-    servo2.write(60);
-    delay(1000);
-  }
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+        #endif
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+    interpolation(ypr[1] * 180/M_PI, ypr[2] * 180/M_PI, 1, 3);
 }
